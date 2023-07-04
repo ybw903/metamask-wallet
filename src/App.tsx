0bfcdae21ac6b63a0454ec7107c3a19b1b99586c
유병우 <conner903@naver.com>
2023-07-04 18:25:55 +0900

    ✨: 메타마스크 지갑 연결 기능 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { popupRegistry } from './store/popup';
 import usePopup from './hooks/store/usePopup';
+import useMetaMask from './hooks/store/useMetaMask';
 import Button from './components/Button';
 import Popup from './components/Popup';
 import { TokenDropdown } from './components/Dropdown';
+import WalletProfile from './components/WalletProfile';
 import { BitCoinIcon, EtheriumIcon } from './components/Icons';
 import { TokenDropdownOption } from './components/Dropdown/TokenDropdown';
 
@@ -24,12 +26,31 @@ const items = [
   { icon: <BitCoinIcon />, label: '비트코인' },
 ];
 
+let injectedProvider = false;
+
+if (typeof window.ethereum !== 'undefined') {
+  injectedProvider = true;
+  console.log(window.ethereum);
+}
+
+const isMetaMask = injectedProvider ? window.ethereum.isMetaMask : false;
+
 function App() {
   const { openPopup } = usePopup();
+  const { hasProvider, wallet, getProvider, connectWallet } = useMetaMask();
 
   const [selectedItem, setSelectedItem] = useState<TokenDropdownOption | undefined>();
 
   const [connectErr, setConnectErr] = useState<string | null>(null);
+
+  useEffect(() => {
+    getProvider();
+  }, [getProvider]);
+
+  const handleClickConnectWallet = () => {
+    if (!isMetaMask || !hasProvider) setConnectErr('Metamask 지갑 설치 후, 이용해주세요.');
+    connectWallet();
+  };
 
   const handleSelectItem = (item: TokenDropdownOption) => {
     setSelectedItem(item);
@@ -39,12 +60,11 @@ function App() {
     openPopup(<>팝업0</>);
   };
 
-  // 지갑 연결 취소 시, 에러 핸들링
+  // [TODO] 지갑 연결 취소 시, 에러 핸들링
   return (
     <div className="App">
       <div className="App__header">
-        {/* eslint-disable-next-line @typescript-eslint/no-empty-function */}
-        <Button color="primary" onClick={() => {}}>
+        <Button color="primary" onClick={handleClickConnectWallet}>
           지갑 연결
         </Button>
         <div className="App__btn_group">
@@ -54,6 +74,7 @@ function App() {
       </div>
       <div className="App__body">
         <h3 className="App__title">프로필</h3>
+        <WalletProfile wallet={wallet} />
       </div>
       {connectErr && <div className="App__error">{connectErr}</div>}
       {popupRegistry.map((content, idx) => (

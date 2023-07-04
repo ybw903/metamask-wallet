882030737a136b0571b0d8c542d90c2a54034110
유병우 <conner903@naver.com>
2023-07-04 18:17:35 +0900

    💄 ✨,: App 레이아웃 및 팝업, 드랍다운 표시

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { popupRegistry } from './store/popup';
+import usePopup from './hooks/store/usePopup';
+import Button from './components/Button';
+import Popup from './components/Popup';
+import { TokenDropdown } from './components/Dropdown';
+import { BitCoinIcon, EtheriumIcon } from './components/Icons';
+import { TokenDropdownOption } from './components/Dropdown/TokenDropdown';
+
+import './App.scss';
+
+const items = [
+  { icon: <EtheriumIcon />, label: '이더리움' },
+  { icon: <BitCoinIcon />, label: '비트코인' },
+  { icon: <EtheriumIcon />, label: '이더리움' },
+  { icon: <BitCoinIcon />, label: '비트코인' },
+  { icon: <EtheriumIcon />, label: '이더리움' },
+  { icon: <BitCoinIcon />, label: '비트코인' },
+  { icon: <EtheriumIcon />, label: '이더리움' },
+  { icon: <BitCoinIcon />, label: '비트코인' },
+  { icon: <EtheriumIcon />, label: '이더리움' },
+  { icon: <BitCoinIcon />, label: '비트코인' },
+  { icon: <EtheriumIcon />, label: '이더리움' },
+  { icon: <BitCoinIcon />, label: '비트코인' },
+];
 
 function App() {
+  const { openPopup } = usePopup();
+
+  const [selectedItem, setSelectedItem] = useState<TokenDropdownOption | undefined>();
+
+  const [connectErr, setConnectErr] = useState<string | null>(null);
+
+  const handleSelectItem = (item: TokenDropdownOption) => {
+    setSelectedItem(item);
+  };
+
+  const handleClickOpenPopup = () => {
+    openPopup(<>팝업0</>);
+  };
+
+  // 지갑 연결 취소 시, 에러 핸들링
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App__header">
+        {/* eslint-disable-next-line @typescript-eslint/no-empty-function */}
+        <Button color="primary" onClick={() => {}}>
+          지갑 연결
+        </Button>
+        <div className="App__btn_group">
+          <Button onClick={handleClickOpenPopup}>팝업 열기</Button>
+          <TokenDropdown items={items} selectItem={handleSelectItem} selectedItem={selectedItem} />
+        </div>
+      </div>
+      <div className="App__body">
+        <h3 className="App__title">프로필</h3>
+      </div>
+      {connectErr && <div className="App__error">{connectErr}</div>}
+      {popupRegistry.map((content, idx) => (
+        <Popup priority={idx} key={`popup-${idx}`}>
+          {content}
+        </Popup>
+      ))}
     </div>
   );
 }
